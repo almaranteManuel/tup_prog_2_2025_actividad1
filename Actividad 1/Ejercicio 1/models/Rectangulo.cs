03088fc01495590d03819a096bdc0b59a40a6ea4
almaranteManuel <almarante.manu@gmail.com>
2025-08-07 16:17:43 -0300

    ASDASDAS

---
+++ Actividad 1/Ejercicio 1/models/Rectangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.models
+{
+    internal class Rectangulo
+    {
+        public double Ancho { get; set; }
+        public double Largo { get; set; }
+
+        public Rectangulo(double ancho, double largo)
+        {
+            Ancho = ancho;
+            Largo = largo;
+        }
+
+        public string Describir()
+        {
+            return "Rectangulo";
+        }
+
+        public double CalcularArea()
+        {
+            return Ancho * Largo;
+        }
+    }
+}

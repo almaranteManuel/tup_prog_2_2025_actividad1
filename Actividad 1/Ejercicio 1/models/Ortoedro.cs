f3d5ba7ccf42d2283ffabef863ceeaa07a0d9f38
almaranteManuel <almarante.manu@gmail.com>
2025-08-07 16:35:03 -0300

    eeeehhhh

--- Actividad 1/Ejercicio 1/models/Ortoedro.cs
+++ Actividad 1/Ejercicio 1/models/Ortoedro.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,24 @@ namespace Ejercicio_1.models
 {
     internal class Ortoedro
     {
+        public Rectangulo[] laterales = new Rectangulo[4];
 
+        public Rectangulo[] bases = new Rectangulo[2];
 
+        public Ortoedro(double anchoBase, double ladoComun, double largoLateral)
+        {
+            bases[0] = new Rectangulo(anchoBase, ladoComun);
+            bases[1] = new Rectangulo(anchoBase, ladoComun);
+            laterales[0] = new Rectangulo(ladoComun, largoLateral);
+            laterales[1] = new Rectangulo(ladoComun, largoLateral);
+            laterales[2] = new Rectangulo(ladoComun, largoLateral);
+            laterales[3] = new Rectangulo(ladoComun, largoLateral);
+        }
+        public double calcularArea()
+        {
+            double areaBase = bases[0].CalcularArea();
+            double areaLateral = laterales[0].CalcularArea();
+            return areaBase*2 + areaLateral*4;
+        }
     }
 }
